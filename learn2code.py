9757636438696b9b72f1b74f3bbd23c222b1dff3
Fallon Myers <fnuqdah@gmail.com>
2022-10-16 07:56:10 +0000

    numbers

--- learn2code.py
+++ learn2code.py
@@ -1,12 +1,3 @@
-first_name = "Monty"
-last_name = "Python"
-
-print(first_name+last_name)
-
-print(first_name + " " + last_name)
-
-firstname = "Jane"
-surname ="Doe"
-
-print(f"My first name is {firstname}. My family name is {surname}")
-
+my_int= 50
+sentence = "The total comes to: "
+print(sentence + str(my_int))
